6c9277535ee83d5927faaf6c5083a0f49786e221
cs-ishii <tsubomi.ishii@central-soft.co.jp>
2022-06-14 12:38:28 +0900

    creat_table.sqlの追加

--- db/creat_tag.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `tag` (
-  `TagID` int NOT NULL,
-  `TagName` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`TagID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
--- db/creat_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-  `UserID` int NOT NULL,
-  `UserMail` varchar(45) NOT NULL,
-  `Password` varchar(45) NOT NULL,
-  PRIMARY KEY (`UserID`),
-  UNIQUE KEY `UserID_UNIQUE` (`UserMail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
