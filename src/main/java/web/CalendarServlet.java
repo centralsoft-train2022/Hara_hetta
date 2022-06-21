6a95374a58cbb1a947fd9bed82f2613763f3e4f4
cs-ishii <tsubomi.ishii@central-soft.co.jp>
2022-06-21 13:09:23 +0900

    カレンダー表示

--- src/main/java/web/CalendarServlet.java
+++ src/main/java/web/CalendarServlet.java
@@ -2,6 +2,7 @@ package web;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,28 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import bean.CalenderBean;
 import dao.DBUtil;
+import dao.DishVo;
 import dao.DishsaveDao;
 
 @WebServlet("/CalendarServlet")
 public class CalendarServlet extends HttpServlet {
-	protected void doPost( HttpServletRequest request,HttpServletResponse response	) 
-		throws ServletException, IOException{
-		
-		
-		DBUtil util = new DBUtil();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		Connection con = util.getConnection();
-
-		DishsaveDao savedao = new DishsaveDao(con);
-		CalenderBean caBean = savedao.getDish(getServletName(), getServletInfo(), 0);
-		request.setAttribute("bean", caBean);
-		
-		CalenderBean bean = new CalenderBean();
-		bean.getDishName();
-
-				System.out.println(bean.getBikou());
-				System.out.println(bean.getDishName());
-
+		sql();
 		Map<String, String[]> foodName = new HashMap<String, String[]>();
 		foodName.put("2022-6-16", new String[] { "ラーメン", "寿司", "カレー" });
 		foodName.put("2022-6-17", new String[] { "塩ラーメン", "サーモン", "カツカレー" });
@@ -44,7 +32,31 @@ public class CalendarServlet extends HttpServlet {
 
 		RequestDispatcher disp = request.getRequestDispatcher("calendar.jsp");
 		disp.forward(request, response);
-		}
+	}
+
+	private void sql() {
+		DBUtil util = new DBUtil();
+
+		Connection con = util.getConnection();
+
+		DishsaveDao savedao = new DishsaveDao(con);
+		//CalenderBean caBean = savedao.getDish();
+		//request.setAttribute("bean", caBean);
+		DishVo dishVo = savedao.getDish();
+
+		String getdn = dishVo.getDishName();
+		String getdb = dishVo.getDishBikou();
+		Date getdd = dishVo.getDishDate();
+
+		CalenderBean bean = new CalenderBean();
+		bean.setDishName(getdn);
+		bean.setBikou(getdb);
+		bean.setDishDate(getdd);
+
+		System.out.println(bean.getBikou());
+		System.out.println(bean.getDishName());
+		System.out.println(bean.getDishDate());
+
+	}
 
 }
-
