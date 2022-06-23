09292bb043aaa4e89cedace7fbeadc6ecb079a02
[cs-maeda] <[akihiro.maeda@central-soft.co.jp]>
2022-06-23 14:43:13 +0900

    警告完成

--- src/main/java/web/CalendarServlet.java
+++ src/main/java/web/CalendarServlet.java
@@ -2,8 +2,10 @@ package web;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -14,49 +16,83 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.CalenderBean;
+import bean.WarningBean;
 import dao.DBUtil;
 import dao.DishVo;
 import dao.DishsaveDao;
+import dao.WarningDao;
+import dao.WarningVo;
 
 @WebServlet("/CalendarServlet")
-public class CalendarServlet extends HttpServlet {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+public class CalendarServlet extends HttpServlet
+{
+	protected void doPost( HttpServletRequest request, HttpServletResponse response )
+	        throws ServletException, IOException
+	{
 
-		sql();
-		Map<String, String[]> foodName = new HashMap<String, String[]>();
-		foodName.put("2022-6-16", new String[] { "ラーメン", "寿司", "カレー" });
-		foodName.put("2022-6-17", new String[] { "塩ラーメン", "サーモン", "カツカレー" });
-		foodName.put("2022-6-18", new String[] { "チロル", "アルフォート", "おにぎり" });
-		request.setAttribute("foodName", foodName);
+		WarningBean bean = new WarningBean( );
+		bean.setWarningList( getWarning( ) );
 
-		RequestDispatcher disp = request.getRequestDispatcher("calendar.jsp");
-		disp.forward(request, response);
+		request.setAttribute( "bean", bean );
+
+		sql( );
+		Map<String, String[]> foodName = new HashMap<String, String[]>( );
+		foodName.put( "2022-6-16", new String[]
+		{ "ラーメン", "寿司", "カレー" } );
+		foodName.put( "2022-6-17", new String[]
+		{ "塩ラーメン", "サーモン", "カツカレー" } );
+		foodName.put( "2022-6-18", new String[]
+		{ "チロル", "アルフォート", "おにぎり" } );
+		request.setAttribute( "foodName", foodName );
+
+		RequestDispatcher disp = request.getRequestDispatcher( "calendar.jsp" );
+		disp.forward( request, response );
 	}
 
-	private void sql() {
-		DBUtil util = new DBUtil();
+	private void sql()
+	{
+		DBUtil util = new DBUtil( );
 
-		Connection con = util.getConnection();
+		Connection con = util.getConnection( );
 
-		DishsaveDao savedao = new DishsaveDao(con);
-		//CalenderBean caBean = savedao.getDish();
-		//request.setAttribute("bean", caBean);
-		DishVo dishVo = savedao.getDish();
+		DishsaveDao savedao = new DishsaveDao( con );
+		// CalenderBean caBean = savedao.getDish();
+		// request.setAttribute("bean", caBean);
+		DishVo dishVo = savedao.getDish( );
 
-		String getdn = dishVo.getDishName();
-		String getdb = dishVo.getDishBikou();
-		Date getdd = dishVo.getDishDate();
+		String	getdn	= dishVo.getDishName( );
+		String	getdb	= dishVo.getDishBikou( );
+		Date	getdd	= dishVo.getDishDate( );
 
-		CalenderBean bean = new CalenderBean();
-		bean.setDishName(getdn);
-		bean.setBikou(getdb);
-		bean.setDishDate(getdd);
+		CalenderBean bean = new CalenderBean( );
+		bean.setDishName( getdn );
+		bean.setBikou( getdb );
+		bean.setDishDate( getdd );
 
-		System.out.println(bean.getBikou());
-		System.out.println(bean.getDishName());
-		System.out.println(bean.getDishDate());
+		System.out.println( bean.getBikou( ) );
+		System.out.println( bean.getDishName( ) );
+		System.out.println( bean.getDishDate( ) );
 
 	}
 
+	private List<WarningVo> getWarning()
+	{
+
+		DBUtil db = new DBUtil( );
+
+		try( Connection c = db.getConnection( ) )
+		{
+			WarningDao		warningdao	= new WarningDao( c );
+			List<WarningVo>	WarningList	= warningdao.getWarning1( );
+			WarningList.addAll( warningdao.getWarning2( ) );
+			WarningList.addAll( warningdao.getWarning3( ) );
+
+			return WarningList;
+		}
+		catch( SQLException e )
+		{
+			throw new RuntimeException( e );
+		}
+
+	}
 }
